8b753475c29ebeca45f3fde4d31fad65c5008c4d
FinnDobkin123 <76793532+FinnDobkin123@users.noreply.github.com>
2021-02-13 22:59:10 -0500

    Update whitespace.py

--- whitespace.py
+++ whitespace.py
@@ -8,11 +8,12 @@ url = 'https://github.com/FinnDobkin123/leg_text/blob/main/congress.csv'
 congress_file = pd.read_csv(url, index_col = 0)
 congress_df = pd.DataFrame(data = congress_file)
 congress_df.head(5)
+congress = open('congress_df', 'r')
 
 #Create list of 104th Congress
 early_congress = []
 late_congress = []
-for congress in congress_file:
+for year in congress:
     if ['Congress'] == '104th':
         early_congress.append(str(row['Bill Title']))
     else:
